7675d390a53472418c247d96e5f0ef5f8a829dd3
Kosuke Numata <coursek8814@gmail.com>
2021-07-21 22:26:32 +0900

    :sparkles: 商品名オートコンプリートのサーバー繋ぎ込み

--- src/graphql/types.tsx
+++ src/graphql/types.tsx
@@ -1,228 +1,263 @@
-import * as Apollo from "@apollo/client"
-import { gql } from "@apollo/client"
-
-export type Maybe<T> = T | null
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K]
-}
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]?: Maybe<T[SubKey]> }
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]: Maybe<T[SubKey]> }
-const defaultOptions = {}
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions =  {}
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string
-  String: string
-  Boolean: boolean
-  Int: number
-  Float: number
-}
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
 
 export type Category = {
-  __typename?: "Category"
-  createdAt: Scalars["String"]
-  enable: Scalars["Boolean"]
-  id: Scalars["ID"]
-  name: Scalars["String"]
-  updatedAt: Scalars["String"]
-  user: User
-}
+  __typename?: 'Category';
+  createdAt: Scalars['String'];
+  enable: Scalars['Boolean'];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  updatedAt: Scalars['String'];
+  user: User;
+};
 
 export type ExpenseHistory = {
-  __typename?: "ExpenseHistory"
-  createdAt: Scalars["String"]
-  expense: Scalars["Int"]
-  id: Scalars["ID"]
-  updatedAt: Scalars["String"]
-  user: User
-}
+  __typename?: 'ExpenseHistory';
+  createdAt: Scalars['String'];
+  expense: Scalars['Int'];
+  id: Scalars['ID'];
+  updatedAt: Scalars['String'];
+  user: User;
+};
 
 export type IncomeHistory = {
-  __typename?: "IncomeHistory"
-  createdAt: Scalars["String"]
-  id: Scalars["ID"]
-  income: Scalars["Int"]
-  updatedAt: Scalars["String"]
-  user: User
-}
+  __typename?: 'IncomeHistory';
+  createdAt: Scalars['String'];
+  id: Scalars['ID'];
+  income: Scalars['Int'];
+  updatedAt: Scalars['String'];
+  user: User;
+};
 
 export type Mutation = {
-  __typename?: "Mutation"
-  createCategory: Category
-  createExpenseHistory: ExpenseHistory
-  createIncomeHistory: IncomeHistory
-  createPayment: Payment
-  createUser: User
-}
+  __typename?: 'Mutation';
+  createCategory: Category;
+  createExpenseHistory: ExpenseHistory;
+  createIncomeHistory: IncomeHistory;
+  createPayment: Payment;
+  createUser: User;
+};
+
 
 export type MutationCreateCategoryArgs = {
-  input: NewCategory
-}
+  input: NewCategory;
+};
+
 
 export type MutationCreateExpenseHistoryArgs = {
-  input: NewExpenseHistory
-}
+  input: NewExpenseHistory;
+};
+
 
 export type MutationCreateIncomeHistoryArgs = {
-  input: NewIncomeHistory
-}
+  input: NewIncomeHistory;
+};
+
 
 export type MutationCreatePaymentArgs = {
-  input: NewPayment
-}
+  input: NewPayment;
+};
+
 
 export type MutationCreateUserArgs = {
-  input: NewUser
-}
+  input: NewUser;
+};
 
 export type NewCategory = {
-  name: Scalars["String"]
-  userId: Scalars["ID"]
-}
+  name: Scalars['String'];
+  userId: Scalars['ID'];
+};
 
 export type NewExpenseHistory = {
-  expense: Scalars["Int"]
-  userId: Scalars["ID"]
-}
+  expense: Scalars['Int'];
+  userId: Scalars['ID'];
+};
 
 export type NewIncomeHistory = {
-  income: Scalars["Int"]
-  userId: Scalars["ID"]
-}
+  income: Scalars['Int'];
+  userId: Scalars['ID'];
+};
 
 export type NewPayment = {
-  amount: Scalars["Int"]
-  categoryId: Scalars["ID"]
-  numberOfProduct: Scalars["Int"]
-  paidOn: Scalars["String"]
-  productName: Scalars["String"]
-  taxIncluded: Scalars["Boolean"]
-  userId: Scalars["ID"]
-}
+  amount: Scalars['Int'];
+  categoryId: Scalars['ID'];
+  numberOfProduct: Scalars['Int'];
+  paidOn: Scalars['String'];
+  productName: Scalars['String'];
+  taxIncluded: Scalars['Boolean'];
+  userId: Scalars['ID'];
+};
 
 export type NewUser = {
-  name: Scalars["String"]
-}
+  name: Scalars['String'];
+};
 
 export type Payment = {
-  __typename?: "Payment"
-  amount: Scalars["Int"]
-  category: Category
-  createdAt: Scalars["String"]
-  id: Scalars["ID"]
-  numberOfProduct: Scalars["Int"]
-  paidOn: Scalars["String"]
-  product: Product
-  taxIncluded: Scalars["Boolean"]
-  updatedAt: Scalars["String"]
-  user: User
-}
+  __typename?: 'Payment';
+  amount: Scalars['Int'];
+  category: Category;
+  createdAt: Scalars['String'];
+  id: Scalars['ID'];
+  numberOfProduct: Scalars['Int'];
+  paidOn: Scalars['String'];
+  product: Product;
+  taxIncluded: Scalars['Boolean'];
+  updatedAt: Scalars['String'];
+  user: User;
+};
 
 export type Product = {
-  __typename?: "Product"
-  createdAt: Scalars["String"]
-  id: Scalars["ID"]
-  name: Scalars["String"]
-  updatedAt: Scalars["String"]
-}
+  __typename?: 'Product';
+  createdAt: Scalars['String'];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  updatedAt: Scalars['String'];
+};
 
 export type Query = {
-  __typename?: "Query"
-  categories: Array<Category>
-  category?: Maybe<Category>
-  expenseHistories: Array<ExpenseHistory>
-  expenseHistory?: Maybe<ExpenseHistory>
-  incomeHistories: Array<IncomeHistory>
-  incomeHistory?: Maybe<IncomeHistory>
-  payment?: Maybe<Payment>
-  payments: Array<Payment>
-  product?: Maybe<Product>
-  products: Array<Product>
-  user?: Maybe<User>
-  users: Array<User>
-}
+  __typename?: 'Query';
+  categories: Array<Category>;
+  category?: Maybe<Category>;
+  expenseHistories: Array<ExpenseHistory>;
+  expenseHistory?: Maybe<ExpenseHistory>;
+  incomeHistories: Array<IncomeHistory>;
+  incomeHistory?: Maybe<IncomeHistory>;
+  payment?: Maybe<Payment>;
+  payments: Array<Payment>;
+  product?: Maybe<Product>;
+  products: Array<Product>;
+  user?: Maybe<User>;
+  users: Array<User>;
+};
+
 
 export type QueryCategoriesArgs = {
-  input?: Maybe<SearchCategories>
-}
+  input?: Maybe<SearchCategories>;
+};
+
 
 export type QueryCategoryArgs = {
-  id: Scalars["ID"]
-}
+  id: Scalars['ID'];
+};
+
 
 export type QueryExpenseHistoryArgs = {
-  id: Scalars["ID"]
-}
+  id: Scalars['ID'];
+};
+
 
 export type QueryIncomeHistoriesArgs = {
-  input?: Maybe<SearchIncomeHistory>
-}
+  input?: Maybe<SearchIncomeHistory>;
+};
+
 
 export type QueryIncomeHistoryArgs = {
-  id: Scalars["ID"]
-}
+  id: Scalars['ID'];
+};
+
 
 export type QueryPaymentArgs = {
-  id: Scalars["ID"]
-}
+  id: Scalars['ID'];
+};
+
 
 export type QueryPaymentsArgs = {
-  input?: Maybe<SearchPayments>
-}
+  input?: Maybe<SearchPayments>;
+};
+
 
 export type QueryProductArgs = {
-  id: Scalars["ID"]
-}
+  id: Scalars['ID'];
+};
+
 
 export type QueryUserArgs = {
-  id: Scalars["ID"]
-}
+  id: Scalars['ID'];
+};
 
 export type SearchCategories = {
-  enable?: Maybe<Scalars["Boolean"]>
-  name?: Maybe<Scalars["String"]>
-  userId: Scalars["ID"]
-}
+  enable?: Maybe<Scalars['Boolean']>;
+  name?: Maybe<Scalars['String']>;
+  userId: Scalars['ID'];
+};
 
 export type SearchIncomeHistory = {
-  beginningOfPeriod?: Maybe<Scalars["String"]>
-  endOfPeriod?: Maybe<Scalars["String"]>
-  userId: Scalars["ID"]
-}
+  beginningOfPeriod?: Maybe<Scalars['String']>;
+  endOfPeriod?: Maybe<Scalars['String']>;
+  userId: Scalars['ID'];
+};
 
 export type SearchPayments = {
-  categoryId?: Maybe<Scalars["ID"]>
-  productName?: Maybe<Scalars["String"]>
-  userId: Scalars["ID"]
-}
+  categoryId?: Maybe<Scalars['ID']>;
+  productName?: Maybe<Scalars['String']>;
+  userId: Scalars['ID'];
+};
 
 export type User = {
-  __typename?: "User"
-  createdAt: Scalars["String"]
-  enable: Scalars["Boolean"]
-  id: Scalars["ID"]
-  name: Scalars["String"]
-  updatedAt: Scalars["String"]
-}
+  __typename?: 'User';
+  createdAt: Scalars['String'];
+  enable: Scalars['Boolean'];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  updatedAt: Scalars['String'];
+};
 
 export type CategoriesQueryVariables = Exact<{
-  userId: Scalars["ID"]
-  enable?: Maybe<Scalars["Boolean"]>
-}>
+  userId: Scalars['ID'];
+  enable?: Maybe<Scalars['Boolean']>;
+}>;
 
-export type CategoriesQuery = { __typename?: "Query" } & {
-  categories: Array<{ __typename?: "Category" } & Pick<Category, "id" | "name">>
-}
+
+export type CategoriesQuery = (
+  { __typename?: 'Query' }
+  & { categories: Array<(
+    { __typename?: 'Category' }
+    & Pick<Category, 'id' | 'name'>
+  )> }
+);
+
+export type PaymentsQueryVariables = Exact<{
+  userId: Scalars['ID'];
+  productName?: Maybe<Scalars['String']>;
+}>;
+
+
+export type PaymentsQuery = (
+  { __typename?: 'Query' }
+  & { payments: Array<(
+    { __typename?: 'Payment' }
+    & { category: (
+      { __typename?: 'Category' }
+      & Pick<Category, 'id' | 'name'>
+    ), product: (
+      { __typename?: 'Product' }
+      & Pick<Product, 'name'>
+    ) }
+  )> }
+);
+
 
 export const CategoriesDocument = gql`
-  query categories($userId: ID!, $enable: Boolean) {
-    categories(input: { userId: $userId, enable: $enable }) {
-      id
-      name
-    }
+    query categories($userId: ID!, $enable: Boolean) {
+  categories(input: {userId: $userId, enable: $enable}) {
+    id
+    name
   }
-`
+}
+    `;
 
 /**
  * __useCategoriesQuery__
@@ -241,37 +276,56 @@ export const CategoriesDocument = gql`
  *   },
  * });
  */
-export function useCategoriesQuery(
-  baseOptions: Apollo.QueryHookOptions<
-    CategoriesQuery,
-    CategoriesQueryVariables
-  >,
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-  return Apollo.useQuery<CategoriesQuery, CategoriesQueryVariables>(
-    CategoriesDocument,
-    options,
-  )
+export function useCategoriesQuery(baseOptions: Apollo.QueryHookOptions<CategoriesQuery, CategoriesQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<CategoriesQuery, CategoriesQueryVariables>(CategoriesDocument, options);
+      }
+export function useCategoriesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<CategoriesQuery, CategoriesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<CategoriesQuery, CategoriesQueryVariables>(CategoriesDocument, options);
+        }
+export type CategoriesQueryHookResult = ReturnType<typeof useCategoriesQuery>;
+export type CategoriesLazyQueryHookResult = ReturnType<typeof useCategoriesLazyQuery>;
+export type CategoriesQueryResult = Apollo.QueryResult<CategoriesQuery, CategoriesQueryVariables>;
+export const PaymentsDocument = gql`
+    query payments($userId: ID!, $productName: String) {
+  payments(input: {userId: $userId, productName: $productName}) {
+    category {
+      id
+      name
+    }
+    product {
+      name
+    }
+  }
 }
+    `;
 
-export function useCategoriesLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<
-    CategoriesQuery,
-    CategoriesQueryVariables
-  >,
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-  return Apollo.useLazyQuery<CategoriesQuery, CategoriesQueryVariables>(
-    CategoriesDocument,
-    options,
-  )
-}
-
-export type CategoriesQueryHookResult = ReturnType<typeof useCategoriesQuery>
-export type CategoriesLazyQueryHookResult = ReturnType<
-  typeof useCategoriesLazyQuery
->
-export type CategoriesQueryResult = Apollo.QueryResult<
-  CategoriesQuery,
-  CategoriesQueryVariables
->
+/**
+ * __usePaymentsQuery__
+ *
+ * To run a query within a React component, call `usePaymentsQuery` and pass it any options that fit your needs.
+ * When your component renders, `usePaymentsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = usePaymentsQuery({
+ *   variables: {
+ *      userId: // value for 'userId'
+ *      productName: // value for 'productName'
+ *   },
+ * });
+ */
+export function usePaymentsQuery(baseOptions: Apollo.QueryHookOptions<PaymentsQuery, PaymentsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<PaymentsQuery, PaymentsQueryVariables>(PaymentsDocument, options);
+      }
+export function usePaymentsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<PaymentsQuery, PaymentsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<PaymentsQuery, PaymentsQueryVariables>(PaymentsDocument, options);
+        }
+export type PaymentsQueryHookResult = ReturnType<typeof usePaymentsQuery>;
+export type PaymentsLazyQueryHookResult = ReturnType<typeof usePaymentsLazyQuery>;
+export type PaymentsQueryResult = Apollo.QueryResult<PaymentsQuery, PaymentsQueryVariables>;
