78ab5e02885f2ff26d9ef43b006e0421a04f0e0f
Kosuke Numata <coursek8814@gmail.com>
2021-07-24 22:01:03 +0900

    :sparkles: カテゴリー作成ページとメニューにリンクを追加

---
+++ src/pages/categories/new/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+import React from "react"
+
+export default function New() {
+  return (
+    <>
+      <Head>
+        <title>カテゴリーの作成 | HABook</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
