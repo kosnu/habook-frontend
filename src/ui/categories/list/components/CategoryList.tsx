31c94a75f522431a0d1c9e2b1766b82badd53dfa
Kosuke Numata <coursek8814@gmail.com>
2021-08-12 01:37:09 +0900

    :sparkles: カテゴリー一覧に追加読み込みを実装

--- src/ui/categories/list/components/CategoryList.tsx
+++ src/ui/categories/list/components/CategoryList.tsx
@@ -1,28 +1,86 @@
 import { List, Menu, MenuItem } from "@material-ui/core"
-import React from "react"
-import { CategoryListItemFragment } from "../../../../graphql/types"
+import React, { useCallback } from "react"
+import InfiniteScroll from "react-infinite-scroll-component"
+import {
+  CategoriesListQuery,
+  CategoriesListQueryVariables,
+  useCategoriesListQuery,
+} from "../../../../graphql/types"
+import { LoadingCircular } from "../../../common/components/LoadingCircular"
+import { useLoginUser } from "../../../common/hooks/useLoginUser"
 import { useAnchorElement } from "../hooks/useAnchorElement"
 import { CategoryItem } from "./CategoryItem"
 
-interface CategoriesListProps {
-  categories: ReadonlyArray<CategoryListItemFragment>
-}
-
-export function CategoryList({ categories }: CategoriesListProps) {
+export function CategoryList() {
+  const { userId } = useLoginUser()
+  const { data, fetchMore, loading } = useCategoriesListQuery({
+    variables: { userId: userId, enable: true, limit: 30 },
+  })
   const { anchorEl, openMenu, closeMenu } = useAnchorElement()
+
+  const pageInfo = (data?.categories && data.categories.pageInfo) ?? null
+  const categories =
+    (data?.categories &&
+      data.categories.edges
+        .filter((value): value is NonNullable<typeof value> => !!value)
+        .map((edge) => edge.node)) ??
+    []
+
+  const handleMoreFetch = useCallback(async () => {
+    return await fetchMore<CategoriesListQuery, CategoriesListQueryVariables>({
+      variables: {
+        cursor: (pageInfo && pageInfo.endCursor) ?? "",
+      },
+      updateQuery: (
+        previousQueryResult,
+        { fetchMoreResult },
+      ): CategoriesListQuery => {
+        if (!fetchMoreResult || !fetchMoreResult.categories)
+          return previousQueryResult
+        const previousEdges =
+          (previousQueryResult?.categories &&
+            previousQueryResult.categories.edges) ??
+          []
+        const nextEdges =
+          (fetchMoreResult?.categories &&
+            fetchMoreResult.categories.edges
+              .filter((value): value is NonNullable<typeof value> => !!value)
+              .map((edge) => ({
+                __typename: edge.__typename,
+                node: edge.node,
+              }))) ??
+          []
+
+        return {
+          ...fetchMoreResult,
+          categories: {
+            ...fetchMoreResult.categories,
+            edges: [...previousEdges, ...nextEdges],
+          },
+        }
+      },
+    })
+  }, [fetchMore, pageInfo])
 
   return (
     <>
       <List>
-        {categories.map((category, index) => {
-          return (
-            <CategoryItem
-              key={index}
-              category={category}
-              onMenuOpen={openMenu}
-            />
-          )
-        })}
+        <InfiniteScroll
+          dataLength={categories.length}
+          hasMore={(pageInfo && pageInfo.hasNextPage) ?? false}
+          next={handleMoreFetch}
+          loader={<LoadingCircular loading={loading} />}
+        >
+          {categories.map((category, index) => {
+            return (
+              <CategoryItem
+                key={index}
+                category={category}
+                onMenuOpen={openMenu}
+              />
+            )
+          })}
+        </InfiniteScroll>
       </List>
       <Menu
         id={`category-item-menu`}
@@ -36,6 +94,7 @@ export function CategoryList({ categories }: CategoriesListProps) {
           削除
         </MenuItem>
       </Menu>
+      <LoadingCircular loading={loading} />
     </>
   )
 }
--- src/ui/categories/list/components/CategoryListTemplate.tsx
+++ src/ui/categories/list/components/CategoryListTemplate.tsx
@@ -1,33 +1,17 @@
 import { css } from "@emotion/react"
 import { Container, Divider, Typography } from "@material-ui/core"
 import React from "react"
-import { useCategoriesListQuery } from "../../../../graphql/types"
-import { LoadingCircular } from "../../../common/components/LoadingCircular"
-import { useLoginUser } from "../../../common/hooks/useLoginUser"
 import { theme } from "../../../theme"
 import { CategoryList } from "./CategoryList"
 
 export function CategoryListTemplate() {
-  const { userId } = useLoginUser()
-  const { data, loading } = useCategoriesListQuery({
-    variables: { userId: userId, enable: true, limit: 30 },
-  })
-
-  const categories =
-    (data?.categories &&
-      data.categories.edges
-        .filter((value): value is NonNullable<typeof value> => !!value)
-        .map((edge) => edge.node)) ??
-    []
-
   return (
     <>
       <Container css={wrapperStyle} maxWidth={"md"}>
         <Typography variant={"h5"}>カテゴリー一覧</Typography>
         <Divider variant={"fullWidth"} />
-        <CategoryList categories={categories} />
+        <CategoryList />
       </Container>
-      <LoadingCircular loading={loading} />
     </>
   )
 }
