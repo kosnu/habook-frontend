e09fefb42efa627fa2eaa8ee95fe63dd66f246c0
Kosuke Numata <coursek8814@gmail.com>
2021-08-16 20:47:04 +0900

    :fire: Drawerを削除

--- src/ui/navigation/components/Header.tsx
+++ src/ui/navigation/components/Header.tsx
@@ -1,33 +1,19 @@
-import { AppBar, IconButton, Toolbar } from "@material-ui/core"
-import { Menu as MenuIcon } from "@material-ui/icons"
+import { AppBar, Toolbar } from "@material-ui/core"
 import React from "react"
-import { useDrawer } from "../hooks/useDrawer"
 import { AccountMenuButton } from "./AccountMenuButton"
 import { AdditionalMenuButton } from "./AdditionalMenuButton"
-import { HeaderDrawer } from "./HeaderDrawer"
 import { HeaderTitle } from "./HeaderTitle"
 
 export function Header() {
-  const { openDrawer, onDrawerOpen, onDrawerClose } = useDrawer()
-
   return (
     <>
       <AppBar position="static" color={"default"}>
         <Toolbar>
-          <IconButton
-            edge="start"
-            color="primary"
-            aria-label="menu"
-            onClick={onDrawerOpen}
-          >
-            <MenuIcon />
-          </IconButton>
           <HeaderTitle />
           <AdditionalMenuButton />
           <AccountMenuButton />
         </Toolbar>
       </AppBar>
-      <HeaderDrawer open={openDrawer} onClose={onDrawerClose} />
     </>
   )
 }
